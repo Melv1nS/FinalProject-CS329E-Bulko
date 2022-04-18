daca57aa3370a0a75f23a92aeeb7c5acff6744b9
Melvin Sureshbabu <melvin.sureshbabu@gmail.com>
2022-04-18 17:35:47 -0500

    signin

--- signin.php
+++ signin.php
@@ -1,33 +1,34 @@
 <?php
   $user = $_POST['user'];
   $pass = $_POST['pw'];
-  $login_state = false;
-  echo($login_state);
+  
+  echo($user);
+  echo($pass);
 
-  $pf = explode("\n", file_get_contents("signup.txt"));
+  // $pf = explode("\n", file_get_contents("signup.txt"));
   
   
-  if (isset($_POST["btn1"])){
+  // if (isset($_POST["btn1"])){
       
-    if(empty($user || $pass)){
-      echo("<script>alert('There are empty fields!')</script>");
-    }
-    else{
-      $up = $user . ":" . $pass;
-      for ($i=0; $i<count($pf);$i++){
-          if($up == $pf[$i]){
+  //   if(empty($user || $pass)){
+  //     echo("<script>alert('There are empty fields!')</script>");
+  //   }
+  //   else{
+  //     $up = $user . ":" . $pass;
+  //     for ($i=0; $i<count($pf);$i++){
+  //         if($up == $pf[$i]){
               
-              $login_state = true;
-              setcookie("user_pass",$up,time()+120,"/");
-              header('Location: explore.html');
-          }
+  //             $login_state = true;
+  //             setcookie("user_pass",$up,time()+120,"/");
+  //             header('Location: explore.html');
+  //         }
   
-      }
-      if ($login_state == false){
-          echo("<script>alert('Wrong Username or Password')</script>"); 
-      }
-    }
-  }
+  //     }
+  //     if ($login_state == false){
+  //         echo("<script>alert('Wrong Username or Password')</script>"); 
+  //     }
+  //   }
+  // }
 ?>
 
 
