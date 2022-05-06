1bb8e3eb13e9115d6f6964df02f15d80b9ec6ca1
“Shaojie <houshaojie0619@gmail.com>
2022-05-05 22:24:58 -0500

    user in db

--- signin.php
+++ signin.php
@@ -1,28 +1,53 @@
-<?php
+<?php   
+  $state = false;
+  $cstate = false;
+  $server = "spring-2022.cs.utexas.edu";
+  $dbuser   = "cs329e_bulko_shaojie";
+  $dbpwd    = "and!manual&During";
+  $dbName = "cs329e_bulko_shaojie";
+
+  $mysqli = new mysqli ($server, $dbuser, $dbpwd, $dbName);
+
   $user = $_POST['user'];
   $pass = $_POST['pw'];
 
-  $pf = explode("\n", file_get_contents("signup.txt"));
-  $login_state = false;
+  $show = "SELECT username FROM foodieuser WHERE username='".$user."';";
+  $compare = "SELECT pwd from foodieuser WHERE username = '".$user."';";
+  $insert = "INSERT INTO foodieuser (username, pwd) VALUES ('$user', '$pass');";
+  $update = "UPDATE foodieuser SET pwd = '$pass' WHERE username = '$user';";
 
-  if(empty($user) || empty($pass)){
-    echo("<script>alert('There are empty fields!')</script>");
-  }
-  else{
-    $up = $user . ":" . $pass;
-    for ($i=0; $i<count($pf);$i++){
-        if($up == $pf[$i]){
-            $login_state = true;
-            setcookie("user", $user, time()+3600, "/");
-            header('Location: home.php');
-        }
+  // $pf = explode("\n", file_get_contents("signup.txt"));
+  $login_state = false;
+  if (isset($_POST['submit2'])){
+    if(empty($user) || empty($pass)){
+      echo("<script>alert('There are empty fields!')</script>");
     }
+    else{
+      if ($result = $mysqli -> query($compare)) {
+        while ($row = $result -> fetch_row()) {
+          if ($row[0] == $pass){
+            $cstate = true;
+          }    
+      }
+    }
+      // $up = $user . ":" . $pass;
+      // for ($i=0; $i<count($pf);$i++){
+      //     if($up == $pf[$i]){
+      //         $login_state = true;
+    if ($cstate == true){
+      $login_state = true;
+      setcookie("user", $user, time()+3600, "/");
+      header('Location: home.php');
+    }
+          // }
+      // }
 
     if ($login_state == false){
       echo "<script>
-              alert('Invalid Username or Password');
-              window.location.href='index.html';
-            </script>";
+                alert('Invalid Username or Password');
+                window.location.href='index.html';
+              </script>";
+      }
     }
-  }
+}
 ?>
--- signup.php
+++ signup.php
@@ -1,28 +1,61 @@
 <?php
+    $state = false;
+    $cstate = false;
+    $server = "spring-2022.cs.utexas.edu";
+    $dbuser   = "cs329e_bulko_shaojie";
+    $dbpwd    = "and!manual&During";
+    $dbName = "cs329e_bulko_shaojie";
 
-  $user = $_POST['user'];
-  $pass = $_POST['pw'];
-  $repass = $_POST['pwconfirm'];
+    $mysqli = new mysqli ($server, $dbuser, $dbpwd, $dbName);
+
+    $user = $_POST['user'];
+    $pass = $_POST['pw'];
+    $repass = $_POST['pwconfirm'];
+
+    $show = "SELECT username FROM foodieuser WHERE username='".$user."';";
+    $compare = "SELECT pwd from foodieuser WHERE username = '".$user."';";
+    $insert = "INSERT INTO foodieuser (username, pwd) VALUES ('$user', '$pass');";
+    $update = "UPDATE foodieuser SET pwd = '$pass' WHERE username = '$user';";
 
   // echo $user;
   // echo $pass;
   // echo $repass;
-
-  if(preg_match('/^[a-zA-Z0-9]{5,}$/', $user) && $pass == $repass) { // for english chars + numbers only
-    // valid username, alphanumeric & longer than or equals 5 chars
-    $up = $user . ":" . $pass . "\n";
-    $myfile = file_put_contents('signup.txt', $up.PHP_EOL , FILE_APPEND | LOCK_EX);
-
-    //make a cookie
-    setcookie("user", $user, time()+3600, "/");
-    header("Location: home.php");
-  }
-  else {
-    echo "<script>
-            alert('Invalid Username or Password');
-            window.location.href='signup.html';
+  if (isset($_POST['submit'])){
+    if(preg_match('/^[a-zA-Z0-9]{5,}$/', $user) && $pass == $repass) { // for english chars + numbers only
+        // valid username, alphanumeric & longer than or equals 5 chars
+        // $up = $user . ":" . $pass . "\n";
+        // $myfile = file_put_contents('signup.txt', $up.PHP_EOL , FILE_APPEND | LOCK_EX);
+        if ($result = $mysqli -> query($show)) {
+            while ($row = $result -> fetch_row()) {
+              // printf ($row[0]."<br>");
+              // printf ($user."<br>");
+              if ($row[0] == $user){
+                $state = true;
+              }             
+            }
+          }
+          if ($state == false){
+            $result = $mysqli->query($insert);
+            echo "New user registered";
+            setcookie("user", $user, time()+3600, "/");
+            header("Location: home.php");
+          }
+          else{
+              echo "<script>
+              alert('User has been existed');
+              window.location.href='signup.html';
           </script>";
-  }
+          }        
+
+        //make a cookie
+    }
+    else {
+        echo "<script>
+                alert('Invalid Username or Password');
+                window.location.href='signup.html';
+            </script>";
+    }
+}
 
   // echo '<script> usercheck({'.$user.'}) </script>'
 
